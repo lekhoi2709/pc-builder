491fb9abbdca6c7a09ed59e3cb568b521a836164
lekhoi2709 <ledinhkhoimd@gmail.com>
2025-06-30 08:03:42 +0700

    feat(front-end): manage zustand and react-router

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { create } from "zustand";
+
+type Store = {
+  count: number;
+  increment: () => void;
+};
+
+const useStore = create<Store>((set) => {
+  return {
+    count: 0,
+    increment: () => set((state) => ({ count: state.count + 1 })),
+  };
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { count, increment } = useStore();
 
   return (
     <>
@@ -18,9 +30,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={increment}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +39,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
